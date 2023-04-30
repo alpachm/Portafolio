4b9c728abdb67cefc82e8aaaa9c30b7743a31488
alpachm <alexpacheco407@gmail.com>
2023-04-30 16:23:12 -0500

    icons node postgresql

--- src/components/Home/Conocimiento.jsx
+++ src/components/Home/Conocimiento.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react'
-import './styles/conocimiento.css'
+import React, { useState } from "react";
+import "./styles/conocimiento.css";
 
 const Conocimiento = () => {
-
-  const [isBox, setIsBox] = useState(true)
+  const [isBox, setIsBox] = useState(true);
 
   // window.addEventListener("scroll", () => {
 
@@ -15,45 +14,53 @@ const Conocimiento = () => {
   // })
 
   return (
-    <div className='conocimiento'>
+    <div className="conocimiento">
       <div className="content-conocimiento grilla hiddenRight">
-        <h3 className='sub-title'>Conocimiento</h3>
+        <h3 className="sub-title">Conocimiento</h3>
         <div className="box-conocimiento">
-
-          <div className={`box ${isBox && 'show-box'}`}>
-            <i className='bx bxl-html5' ></i>
+          <div className={`box ${isBox && "show-box"}`}>
+            <i className="bx bxl-html5"></i>
             <h4>HTML</h4>
           </div>
 
-          <div className={`box ${isBox && 'show-box'}`}>
-            <i className='bx bxl-css3'></i>
+          <div className={`box ${isBox && "show-box"}`}>
+            <i className="bx bxl-css3"></i>
             <h4>CSS</h4>
           </div>
 
-          <div className={`box ${isBox && 'show-box'}`}>
-            <i className='bx bxl-javascript' ></i>
-            <h4>JavasCript</h4>
+          <div className={`box ${isBox && "show-box"}`}>
+            <i className="bx bxl-javascript"></i>
+            <h4>JavaScript</h4>
           </div>
 
-          <div className={`box ${isBox && 'show-box'}`}>
-            <i className='bx bxl-react' ></i>
-            <h4>REACT JS</h4>
+          <div className={`box ${isBox && "show-box"}`}>
+            <i className="bx bxl-react"></i>
+            <h4>React js</h4>
           </div>
 
-          <div className={`box ${isBox && 'show-box'}`}>
-            <i className='bx bxl-github' ></i>
+          <div className={`box ${isBox && "show-box"}`}>
+            <i className="bx bxl-github"></i>
             <h4>GitHub</h4>
           </div>
 
-          <div className={`box ${isBox && 'show-box'}`}>
-            <i className='bx bxl-redux' ></i>
+          <div className={`box ${isBox && "show-box"}`}>
+            <i className="bx bxl-redux"></i>
             <h4>Redux</h4>
           </div>
 
+          <div className={`box ${isBox && "show-box"}`}>
+            <i className="bx bxl-nodejs"></i>
+            <h4>Node js</h4>
+          </div>
+
+          <div className={`box ${isBox && "show-box"}`}>
+            <i className="bx bxl-postgresql"></i>
+            <h4>PostgreSQL</h4>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Conocimiento
+export default Conocimiento;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,72 +1,66 @@
-import React, { useEffect } from 'react'
-import './styles/homePage.css'
-import AboutMe from '../components/Home/AboutMe'
-import Conocimiento from '../components/Home/Conocimiento'
-import Main from '../components/Home/Main'
-import Skills from '../components/Home/Skills'
-import Works from '../components/Home/Works'
-import Contact from '../components/shared/Contact'
-import Header from '../components/shared/Header'
-import NavBar from '../components/shared/NavBar'
-import Footer from '../components/shared/Footer'
-import TopArrow from '../components/shared/TopArrow'
+import React, { useEffect } from 'react';
+import './styles/homePage.css';
+import AboutMe from '../components/Home/AboutMe';
+import Conocimiento from '../components/Home/Conocimiento';
+import Main from '../components/Home/Main';
+import Skills from '../components/Home/Skills';
+import Works from '../components/Home/Works';
+import Contact from '../components/shared/Contact';
+import Header from '../components/shared/Header';
+import NavBar from '../components/shared/NavBar';
+import Footer from '../components/shared/Footer';
+import TopArrow from '../components/shared/TopArrow';
 
 const HomePage = ({ fullLoading }) => {
+  useEffect(() => {
+    const elementsRight = document.querySelectorAll('.hiddenRight');
+    const elementLeft = document.querySelectorAll('.hiddenLeft');
+    const skills = document.querySelectorAll('.skill-hijo');
 
-    useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            if (!fullLoading) {
+              if (entry.target.classList.value.includes('hiddenRight')) {
+                entry.target.classList.add('show-hiddenRight');
+              }
+              if (entry.target.classList.value.includes('hiddenLeft')) {
+                entry.target.classList.add('show-hiddenLeft');
+              }
+              if (entry.target.classList.value.includes('skill-hijo')) {
+                entry.target.classList.add('show-skill-hijo');
+              }
+            }
+          } else {
+            if (entry.target.classList.value.includes('skill-hijo')) {
+              entry.target.classList.remove('show-skill-hijo');
+            }
+          }
+        });
+      },
+      { threshold: 0.2 }
+    );
 
-        const elementsRight = document.querySelectorAll('.hiddenRight')
-        const elementLeft = document.querySelectorAll('.hiddenLeft')
-        const skills = document.querySelectorAll('.skill-hijo')
+    elementsRight.forEach((element) => observer.observe(element));
+    elementLeft.forEach((element) => observer.observe(element));
+    skills.forEach((skill) => observer.observe(skill));
+  }, [fullLoading]);
 
-        const observer = new IntersectionObserver(entries => {
+  return (
+    <div className="home-page">
+      <Header />
+      <Main />
+      <AboutMe />
+      <Conocimiento />
+      <Skills />
+      <Works />
+      <Contact />
+      <Footer />
+      <NavBar />
+      <TopArrow />
+    </div>
+  );
+};
 
-            entries.forEach(entry => {
-
-                if (entry.isIntersecting) {
-
-                    if (!fullLoading) {
-                        if (entry.target.classList.value.includes('hiddenRight')) {
-                            entry.target.classList.add('show-hiddenRight')
-                        }
-                        if (entry.target.classList.value.includes('hiddenLeft')) {
-                            entry.target.classList.add('show-hiddenLeft')
-                        }
-                        if (entry.target.classList.value.includes('skill-hijo')) {
-                            entry.target.classList.add('show-skill-hijo')
-                        }
-                    }
-                } else {
-
-                    if (entry.target.classList.value.includes('skill-hijo')) {
-                        entry.target.classList.remove('show-skill-hijo')
-                    }
-
-                }
-            })
-        }, { threshold: .2 })
-
-        elementsRight.forEach(element => observer.observe(element))
-        elementLeft.forEach(element => observer.observe(element))
-        skills.forEach(skill => observer.observe(skill))
-
-    }, [fullLoading])
-
-    return (
-        <div className='home-page'>
-            <Header />
-            <Main />
-            <AboutMe />
-            <Conocimiento />
-            <Skills />
-            <Works />
-            <Contact />
-            <Footer />
-            <NavBar />
-            <TopArrow />
-
-        </div>
-    )
-}
-
-export default HomePage
+export default HomePage;
